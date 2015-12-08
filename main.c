a8e7e2fdf0560b70dc79165a692362d6715162f9
nikitawanjale <wanjale@unlv.nevada.edu>
2015-12-07 23:55:15 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,47 +1,72 @@
+/* 
+Lab12_T02 Modify the original code to print Capital Letters when small letters are entered and vice
+versa on the hyperterm/serialchart.
+*/
+
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h> // for char operations
+#include "inc/hw_ints.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
 #include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
 #include "driverlib/pin_map.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
 
-int main(void) {
+void UARTIntHandler();
 
-	//Set the system clock
+int main(void) {
+	char *prompt = "Enter Text: ";
+	int i;
+	
 	SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
 
-	//Enable the peripheral UART0 and GPIOA
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
-    //Configure PA0 as Rx and PA1 as TX and set the pin type as UART
     GPIOPinConfigure(GPIO_PA0_U0RX);
     GPIOPinConfigure(GPIO_PA1_U0TX);
     GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
-    //Set the Baud rate as 115200, 8 data bits, 1 stop bit and no parity bit
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); //enable GPIO port for LED
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2); //enable pin for LED PF2
+
     UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
         (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
 
-    //Send the characters 'Enter Text:'
-    UARTCharPut(UART0_BASE, 'E');
-    UARTCharPut(UART0_BASE, 'n');
-    UARTCharPut(UART0_BASE, 't');
-    UARTCharPut(UART0_BASE, 'e');
-    UARTCharPut(UART0_BASE, 'r');
-    UARTCharPut(UART0_BASE, ' ');
-    UARTCharPut(UART0_BASE, 'T');
-    UARTCharPut(UART0_BASE, 'e');
-    UARTCharPut(UART0_BASE, 'x');
-    UARTCharPut(UART0_BASE, 't');
-    UARTCharPut(UART0_BASE, ':');
+    IntMasterEnable(); //enable processor interrupts
+    IntEnable(INT_UART0); //enable the UART interrupt
+    UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT); //only enable RX and TX interrupts
+
+    for (i = 0; prompt[i] != '\0'; i++) 	// Print prompt at start of program
+    	UARTCharPut(UART0_BASE, prompt[i]);
     UARTCharPut(UART0_BASE, ' ');
 
-    while (1)
+    while(1); //let interrupt handler do the UART echo function
+}
+
+void UARTIntHandler()
+{
+    uint32_t ui32Status;
+    char c;
+
+    ui32Status = UARTIntStatus(UART0_BASE, true); //get interrupt status
+
+    UARTIntClear(UART0_BASE, ui32Status); //clear the asserted interrupts
+
+    while(UARTCharsAvail(UART0_BASE)) //loop while there are chars
     {
-    	if (UARTCharsAvail(UART0_BASE)) UARTCharPut(UART0_BASE, UARTCharGet(UART0_BASE));
+    	c = UARTCharGetNonBlocking(UART0_BASE);
+    	if (isalpha(c))
+    		if (c < 91)
+    			c = tolower(c);
+    		else
+    			c = toupper(c);
+        UARTCharPutNonBlocking(UART0_BASE, c); //echo character
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2); //blink LED
+        SysCtlDelay(SysCtlClockGet() / (1000 * 3)); //delay ~1 msec
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0); //turn off LED
     }
-
 }
